fd50bdca1c9ba1e6f00eed553ac64cb2f9bce0cc
Sangmin <23mean@naver.com>
2022-11-27 22:36:47 +0900

    feature: default App 및 routing 설정

---
+++ src/routers/index.tsx
@@ -0,0 +1,13 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+const Routers = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<></>} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default Routers;
