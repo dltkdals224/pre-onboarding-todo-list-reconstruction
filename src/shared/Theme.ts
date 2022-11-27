878e30f34f77e72713ec10a401c424b68442576d
Sangmin <23mean@naver.com>
2022-11-27 22:36:16 +0900

    feature: 프로젝트 스타일 설정

---
+++ src/shared/Theme.ts
@@ -0,0 +1,13 @@
+/* eslint-disable import/no-anonymous-default-export */
+import { css } from "styled-components";
+
+export default {
+  FLEX_CENTER: css`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  `,
+
+  BLACK: "#00000",
+  WHITE: "#FFFFF",
+};
