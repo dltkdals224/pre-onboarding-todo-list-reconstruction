ddbf2f669277dc7f8b46deba6bdeb2005d6f017b
Sangmin <23mean@naver.com>
2022-12-05 21:21:38 +0900

    feature: todoList page내 useQuery READ

--- src/pages/todoList/index.tsx
+++ src/pages/todoList/index.tsx
@@ -1,9 +1,36 @@
-import React from "react";
+import { useQuery } from "react-query";
+
+import TodoInput from "../../components/TodoList/TodoInput";
+import TodoItem from "../../components/TodoList/TodoItem";
+
+import { getTodoApi } from "../../apis/todo";
 
 import * as Style from "./style";
 
 const TodoList = () => {
-  return <Style.Article>TodoList Page</Style.Article>;
+  // custom hook 처리 가능
+  const useGetTodoListQuery = () => {
+    return useQuery({
+      queryKey: ["todoList"],
+      queryFn: () => {
+        return getTodoApi();
+      },
+    });
+  };
+  //
+  const { data: todoList, isLoading, isError } = useGetTodoListQuery();
+
+  return (
+    <Style.Article>
+      <TodoInput />
+
+      <Style.TodoList>
+        {todoList?.data.map((todo: any) => {
+          return <TodoItem key={todo.id} data={todo} />;
+        })}
+      </Style.TodoList>
+    </Style.Article>
+  );
 };
 
 export default TodoList;
